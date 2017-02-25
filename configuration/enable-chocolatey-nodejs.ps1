7f3ae57dd7f8be87ffbddfed3a0a9c1d128869ed
André Geuze <andre.geuze@avanade.com>
2017-02-25 13:12:22 +0100

    added powershell script for installation of chocolatey and nodejs

---
+++ configuration/enable-chocolatey-nodejs.ps1
@@ -0,0 +1,30 @@
+﻿param(
+    [switch] $chocolatey,
+    [switch] $nodejs
+)
+
+# Set machine policy unrestricted
+$localMachinePolicy = Get-ExecutionPolicy
+Write-Verbose "Machine policy is $localMachinePolicy"
+Set-ExecutionPolicy Unrestricted
+Write-Verbose "Setting machine policy to Unrestricted"
+
+if ($chocolatey)
+{
+    # --------------------------------------------------------------------
+    # Install Chocolatey
+    # --------------------------------------------------------------------
+    iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+}
+
+if ($nodejs -and $chocolatey)
+{
+    # --------------------------------------------------------------------
+    # Install Nodejs through chocolatey
+    # --------------------------------------------------------------------
+    choco install nodejs.install -y
+}
+
+# Reset machine policy
+Write-Verbose "Setting machine policy back to $localMachinePolicy"
+Set-ExecutionPolicy $localMachinePolicy
