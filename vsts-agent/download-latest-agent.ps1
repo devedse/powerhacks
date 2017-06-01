34f354c79f8ea575e064ffec90f60e4540bc3210
André Geuze <andre.geuze@avanade.com>
2017-06-01 20:39:11 +0200

    Added vsts agent folder with scripts to download the latest agent and register against a vsts project

---
+++ vsts-agent/download-latest-agent.ps1
@@ -0,0 +1,48 @@
+﻿# Source: https://blogs.msdn.microsoft.com/jasonn/2008/06/13/downloading-files-from-the-internet-in-powershell-with-progress/
+
+param(
+    $vstsAgentGithubUrl = "https://api.github.com/repos/microsoft/vsts-agent/releases/latest"
+)
+
+function downloadFile($url, $targetFile)
+{
+    "Downloading $url" 
+    $uri = New-Object "System.Uri" "$url" 
+    $request = [System.Net.HttpWebRequest]::Create($uri) 
+    $request.set_Timeout(15000) #15 second timeout 
+    $response = $request.GetResponse() 
+    $totalLength = [System.Math]::Floor($response.get_ContentLength()/1024) 
+    $responseStream = $response.GetResponseStream() 
+    $targetStream = New-Object -TypeName System.IO.FileStream -ArgumentList $targetFile, Create 
+    $buffer = new-object byte[] 10KB 
+    $count = $responseStream.Read($buffer,0,$buffer.length) 
+    $downloadedBytes = $count 
+    while ($count -gt 0) 
+    {
+        $targetStream.Write($buffer, 0, $count) 
+        $count = $responseStream.Read($buffer,0,$buffer.length) 
+        $downloadedBytes = $downloadedBytes + $count 
+        Write-Progress -activity "Downloading file '$($url.split('/') | Select -Last 1)'" -status "Downloaded ($([System.Math]::Floor($downloadedBytes/1024))K of $($totalLength)K): " -PercentComplete ((([System.Math]::Floor($downloadedBytes/1024)) / $totalLength)  * 100)
+    } 
+    Write-Progress -activity "Finished downloading file '$($url.split('/') | Select -Last 1)'"
+    $targetStream.Flush()
+    $targetStream.Close() 
+    $targetStream.Dispose() 
+    $responseStream.Dispose() 
+} 
+
+# Call github api to get the latest release information
+$response = iwr -Uri "$vstsAgentGithubUrl" -Method Get
+
+# Convert the content to json
+$json = $response.Content | ConvertFrom-Json
+
+# Get the asset containing the agent zip file url
+$asset = $json.assets | where { $_.name -like "*zip" }
+
+# Extract the download url to the latest agent zip
+$latestAgentUrl = $asset.browser_download_url
+"Found url $latestAgentUrl"
+
+# Download the zip and rename
+downloadFile "$latestAgentUrl" "agent.zip"
---
+++ vsts-agent/register-agent.ps1
@@ -0,0 +1,54 @@
+param(
+    $projecturl,
+    $projectname,
+    $pattoken,
+    $deploymentgroupname,
+    $deploymentgrouptags,
+    $windowslogonaccount,
+    $windowslogonpassword,
+    $agentfolder = "vstsagent",
+    $agentworkfolder = "_work"
+)
+
+$ErrorActionPreference="Stop"
+$agentZip="$PSScriptRoot\agent.zip"
+If(-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    throw "Run command in Administrator PowerShell Prompt"
+}
+
+If(Test-Path $env:SystemDrive\$agentfolder\config.cmd)
+{
+    cd "$env:SystemDrive\$agentfolder"
+    .\config.cmd remove --auth PAT --token $pattoken
+    cd "$PSScriptRoot"
+    Remove-Item -Path "$env:SystemDrive\$agentfolder" -Recurse -Force -Confirm:$false
+
+	Add-Type -AssemblyName System.IO.Compression.FileSystem
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($agentZip, "$env:SystemDrive\$agentfolder")
+}
+Else
+{
+	Add-Type -AssemblyName System.IO.Compression.FileSystem
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($agentZip, "$env:SystemDrive\$agentfolder")
+}
+Remove-Item $agentZip
+
+cd "$env:SystemDrive\$agentfolder"
+
+.\config.cmd `
+    --addmachinegrouptags `
+    --machinegroup `
+    --replace `
+    --unattended `
+    --runasservice `
+    --auth PAT `
+    --token $pattoken `
+    --url "$projecturl" `
+    --projectname "$projectname" `
+    --agent $env:COMPUTERNAME `
+    --work "$agentworkfolder" `
+    --machinegroupname "$deploymentgroupname" `
+    --machinegrouptags "$deploymentgrouptags" `
+    --windowslogonaccount "$windowslogonaccount" `
+    --windowslogonpassword "$windowslogonpassword"
