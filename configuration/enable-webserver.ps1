a0b5f538615f3d4d1a1f297f11af647ac69e4e9e
André Geuze <andre.geuze@avanade.com>
2017-06-08 22:50:54 +0200

    updated configuration scripts

--- configuration/enable-webserver.ps1
+++ configuration/enable-webserver.ps1
@@ -26,46 +26,6 @@ configuration EnableIIS
             Ensure = "Present"
             DependsOn = "[WindowsFeature]WebServerRole"
         }
-
-        # Script block to download WebPI MSI from the Azure storage blob
-        Script DownloadWebPIImage
-        {
-            GetScript = {
-                @{
-                    Result = "WebPIInstall"
-                }
-            }
-
-            TestScript = {
-                Test-Path "C:\wpilauncher.exe"
-            }
-
-            SetScript ={
-                $source = "http://go.microsoft.com/fwlink/?LinkId=255386"
-                $destination = "C:\wpilauncher.exe"
-                Invoke-WebRequest $source -OutFile $destination
-            }
-        }
-
-        Package WebPi_Installation
-        {
-            Ensure = "Present"
-            Name = "Microsoft Web Platform Installer 5.0"
-            Path = "C:\wpilauncher.exe"
-            ProductId = '4D84C195-86F0-4B34-8FDE-4A17EB41306A'
-            Arguments = ''
-            DependsOn = @("[Script]DownloadWebPIImage")
-        }
-
-        Package WebDeploy_Installation
-        {
-            Ensure = "Present"
-            Name = "Microsoft Web Deploy 3.6"
-            Path = "$env:ProgramFiles\Microsoft\Web Platform Installer\WebPiCmd-x64.exe"
-            ProductId = ''
-            Arguments = "/install /products:ASPNET_REGIIS_NET4,WDeploy36  /AcceptEula"
-            DependsOn = @("[Package]WebPi_Installation")
-        }
     }
 }
 EnableIIS
